0446b57a2cee99126a9c48f18e0e31a2155a6a34
Lauren Suguro <lsuguro@Laurens-MacBook-Pro.local>
2023-03-10 09:54:58 -0500

    all rides are powered on

--- index.js
+++ index.js
@@ -1,39 +1,79 @@
 //DO NOT RENAME THESE VARIABLES, THEY ARE USED TO RENDER THINGS
-let ride1PowerOn = false
-let ride2PowerOn = false
-let ride3PowerOn = false
-let ride4PowerOn = false
-let greenWireConnected = true
-let yellowWireConnected = true
-let redWireConnected = true
-let waterIsOn = true
+let ride1PowerOn = false;
+let ride2PowerOn = false;
+let ride3PowerOn = false;
+let ride4PowerOn = false;
+let greenWireConnected = true;
+let yellowWireConnected = true;
+let redWireConnected = true;
+let waterIsOn = true;
 //Everything below is fair game for you to power on the rides
 
 //Ride 1 works when:
-    // green wire is connected
-ride1PowerOn = false
+// green wire is connected
+ride1PowerOn = false;
 
-//Ride 2 works when 
-    //green and yellow wires are connected
-ride2PowerOn = false
+if (greenWireConnected) {
+  ride1PowerOn = true;
+}
+
+//Ride 2 works when
+//green and yellow wires are connected
+ride2PowerOn = false;
+
+if (greenWireConnected && redWireConnected) {
+  ride2PowerOn = true;
+}
 
 //Ride 3 works when:
-    // green and red wires are connected
-    // water is on 
-    // yellow wire is not connected
-ride3PowerOn = false
+// green and red wires are connected
+// water is on
+// yellow wire is not connected
+ride3PowerOn = false;
+yellowWireConnected = false;
+
+if (
+  greenWireConnected &&
+  redWireConnected &&
+  waterIsOn &&
+  !yellowWireConnected
+) {
+  ride3PowerOn = true;
+}
 
 //Ride 4 has power when:
-    //ride wire is connected
-ride4PowerOn = false
+//ride wire is connected
+ride4PowerOn = false;
 //Ride 4 is a slot machine
-    //if a 1 is rolled it should return "star"
-    //if a 2 is rolled it should return "car"
-    //if a 3 is rolled it should return "pot of gold"
-    //if a 4 is rolled it should return "clover"
-    //if a 5 is rolled it should return "bowling ball"
-    //if a 6 is rolled it should return "tiger"
-    //if a 7 is rolled it should return "seven"
-function ride4SlotMachine(rolledNumber){
-    return "nothing"
+//if a 1 is rolled it should return "star"
+//if a 2 is rolled it should return "car"
+//if a 3 is rolled it should return "pot of gold"
+//if a 4 is rolled it should return "clover"
+//if a 5 is rolled it should return "bowling ball"
+//if a 6 is rolled it should return "tiger"
+//if a 7 is rolled it should return "seven"
+
+if (redWireConnected) {
+  ride4PowerOn = true;
+}
+
+function ride4SlotMachine(rolledNumber) {
+  switch (rolledNumber) {
+    case 1:
+      return "star";
+    case 2:
+      return "car";
+    case 3:
+      return "pot of gold";
+    case 4:
+      return "clover";
+    case 5:
+      return "bowling ball";
+    case 6:
+      return "tiger";
+    case 7:
+      return "seven";
+    default:
+      break;
+  }
 }
--- power-status.js
+++ power-status.js
@@ -1,74 +1,67 @@
-
-let ride1PowerElement = document.querySelector("#ride-1-power")
-let ride2PowerElement = document.querySelector("#ride-2-power")
-let ride3PowerElement = document.querySelector("#ride-3-power")
-let ride4PowerElement = document.querySelector("#ride-4-power")
+let ride1PowerElement = document.querySelector("#ride-1-power");
+let ride2PowerElement = document.querySelector("#ride-2-power");
+let ride3PowerElement = document.querySelector("#ride-3-power");
+let ride4PowerElement = document.querySelector("#ride-4-power");
 if (ride1PowerOn === true) {
-    ride1PowerElement.style.color = "green"
-    ride1PowerElement.textContent = "Power Status: on"
-}
-else {
-    ride1PowerElement.style.color = "red"
-    ride1PowerElement.textContent = "Power Status: off"
+  ride1PowerElement.style.color = "green";
+  ride1PowerElement.textContent = "Power Status: on";
+} else {
+  ride1PowerElement.style.color = "red";
+  ride1PowerElement.textContent = "Power Status: off";
 }
 
 if (ride2PowerOn === true) {
-    ride2PowerElement.style.color = "green"
-    ride2PowerElement.textContent = "Power Status: on"
-}
-else {
-    ride2PowerElement.style.color = "red"
-    ride2PowerElement.textContent = "Power Status: off"
+  ride2PowerElement.style.color = "green";
+  ride2PowerElement.textContent = "Power Status: on";
+} else {
+  ride2PowerElement.style.color = "red";
+  ride2PowerElement.textContent = "Power Status: off";
 }
 
 if (ride3PowerOn === true) {
-    ride3PowerElement.style.color = "green"
-    ride3PowerElement.textContent = "Power Status: on"
-}
-else {
-    ride3PowerElement.style.color = "red"
-    ride3PowerElement.textContent = "Power Status: off"
+  ride3PowerElement.style.color = "green";
+  ride3PowerElement.textContent = "Power Status: on";
+} else {
+  ride3PowerElement.style.color = "red";
+  ride3PowerElement.textContent = "Power Status: off";
 }
 
 //power is on and all slots work right
-if (ride4PowerOn === true &&
-    ride4SlotMachine(1) == "star" &&
-    ride4SlotMachine(2) == "car" &&
-    ride4SlotMachine(3) == "pot of gold" &&
-    ride4SlotMachine(4) == "clover" &&
-    ride4SlotMachine(5) == "bowling ball" &&
-    ride4SlotMachine(6) == "tiger" &&
-    ride4SlotMachine(7) == "seven"
+if (
+  ride4PowerOn === true &&
+  ride4SlotMachine(1) == "star" &&
+  ride4SlotMachine(2) == "car" &&
+  ride4SlotMachine(3) == "pot of gold" &&
+  ride4SlotMachine(4) == "clover" &&
+  ride4SlotMachine(5) == "bowling ball" &&
+  ride4SlotMachine(6) == "tiger" &&
+  ride4SlotMachine(7) == "seven"
 ) {
-    ride4PowerElement.style.color = "green"
-    ride4PowerElement.textContent = "Power Status: on"
-}
-else {
-    ride4PowerElement.style.color = "red"
-    ride4PowerElement.textContent = "Power Status: off"
+  ride4PowerElement.style.color = "green";
+  ride4PowerElement.textContent = "Power Status: on";
+} else {
+  ride4PowerElement.style.color = "red";
+  ride4PowerElement.textContent = "Power Status: off";
 }
 
-
-let spinButton = document.querySelector("#spin")
-let slot1Div = document.querySelector("#slot-1")
-let slot2Div = document.querySelector("#slot-2")
-let slot3Div = document.querySelector("#slot-3")
+let spinButton = document.querySelector("#spin");
+let slot1Div = document.querySelector("#slot-1");
+let slot2Div = document.querySelector("#slot-2");
+let slot3Div = document.querySelector("#slot-3");
 
 spinButton.addEventListener("click", function () {
-    //if all match up you win
-    let slot1 = ride4SlotMachine(3)
-    slot1Div.textContent = slot1
-    let slot2 = ride4SlotMachine(3)
-    slot2Div.textContent = slot1
-    let slot3 = ride4SlotMachine(3)
-    slot3Div.textContent = slot1
-    let didTheyWin = (slot1 === slot2) && (slot2 === slot3)
+  //if all match up you win
+  let slot1 = ride4SlotMachine(3);
+  slot1Div.textContent = slot1;
+  let slot2 = ride4SlotMachine(3);
+  slot2Div.textContent = slot1;
+  let slot3 = ride4SlotMachine(3);
+  slot3Div.textContent = slot1;
+  let didTheyWin = slot1 === slot2 && slot2 === slot3;
 
-    if (ride4PowerOn && didTheyWin) {
-        setTimeout(() => {
-            window.alert("You won the slot machine")
-        }, 500)
-
-    }
-})
-
+  if (ride4PowerOn && didTheyWin) {
+    setTimeout(() => {
+      window.alert("You won the slot machine");
+    }, 500);
+  }
+});
